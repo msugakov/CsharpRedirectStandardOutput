c5a991963d8d9b7a47fe0bd0bb2d22f1a4628756
alabax <outspirit@gmail.com>
2013-06-27 02:35:50 +0400

    initial commit of FlawedPattern example

---
+++ RedirectStandardOutputLibrary.Tests/FlawedPatternTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+
+namespace RedirectStandardOutputLibrary.Tests
+{
+    public class FlawedPatternTests
+    {
+        private const int DefaultTimeout = 1000;
+
+        private const int Runs = 1000;
+
+        [Fact]
+        public void TestItBreaks()
+        {
+            for (int i = 0; i < Runs; ++i)
+            {
+                string testString = Guid.NewGuid().ToString();
+
+                string standardOutput, standardError;
+
+                int returnCode = FlawedPattern.ExecuteProcess(
+                    "cmd.exe",
+                    string.Format("/c echo {0}", testString),
+                    DefaultTimeout,
+                    out standardOutput,
+                    out standardError);
+
+                Assert.Equal(0, returnCode);
+
+                Assert.Equal(testString, standardOutput.Trim()); // should fail here
+
+                Assert.Equal(string.Empty, standardError);
+            }
+        }
+    }
+}
---
+++ RedirectStandardOutputLibrary/FlawedPattern.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace RedirectStandardOutputLibrary
+{
+    public class FlawedPattern
+    {
+        public static int ExecuteProcess(
+            string fileName,
+            string arguments,
+            int timeout,
+            out string standardOutput,
+            out string standardError)
+        {
+            int exitCode;
+
+            var standardOutputBuilder = new StringBuilder();
+            var standardErrorBuilder = new StringBuilder();
+
+            using (var process = new Process())
+            {
+                process.StartInfo.UseShellExecute = false;
+                process.StartInfo.CreateNoWindow = true;
+                process.StartInfo.RedirectStandardError = true;
+                process.StartInfo.RedirectStandardOutput = true;
+                process.StartInfo.FileName = fileName;
+                process.StartInfo.Arguments = arguments;
+
+                process.OutputDataReceived += (sender, args) =>
+                {
+                    if (args.Data != null)
+                    {
+                        standardOutputBuilder.AppendLine(args.Data);
+                    }
+                };
+                process.ErrorDataReceived += (sender, args) =>
+                {
+                    if (args.Data != null)
+                    {
+                        standardErrorBuilder.AppendLine(args.Data);
+                    }
+                };
+
+                process.Start();
+
+                //Thread.Sleep(100);
+
+                process.BeginOutputReadLine();
+                process.BeginErrorReadLine();
+
+                if (process.WaitForExit(timeout))
+                {
+                    exitCode = process.ExitCode;
+                }
+                else
+                {
+                    process.Kill();
+
+                    throw new TimeoutException("Process wait timeout expired");
+                }
+            }
+
+            standardOutput = standardOutputBuilder.ToString();
+            standardError = standardErrorBuilder.ToString();
+
+            return exitCode;
+        }
+    }
+}
